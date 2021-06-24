8aa74c61e92dcb7cb2eaca51a63f4063bfa05840
Saral33 <princesaral2013@gmail.com>
2021-06-24 13:17:02 +0545

    some fixes

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,42 +1,42 @@
-const asyncHandler = require("express-async-handler");
-const User = require("../Models/userModel");
-const Product = require("../Models/productModel");
-const CheckOut = require("../Models/checkoutModel");
+const asyncHandler = require('express-async-handler');
+const User = require('../Models/userModel');
+const Product = require('../Models/productModel');
+const CheckOut = require('../Models/checkoutModel');
 
 const loginAdmin = asyncHandler(async (req, res) => {
   const { email, password, code } = req.body;
   if (!email || !password || !code) {
-    throw new Error("Every field is required");
+    throw new Error('Every field is required');
   }
   const user = await User.findOne({ email });
   if (!user || !(await user.comparePassword(password, user.password))) {
-    throw new Error("Invalid password or email");
+    throw new Error('Invalid password or email');
   }
-  if (user.role !== "admin") {
-    throw new Error("You arenot admin");
+  if (user.role !== 'admin') {
+    throw new Error('You arenot admin');
   }
   if (code !== process.env.ADMIN_SECRET_key) {
-    throw new Error("Invalid code");
+    throw new Error('Invalid code');
   }
 
   const token = await user.generateToken(3600 * 5);
   res
     .status(200)
-    .cookie("token", token, {
+    .cookie('token', token, {
       httpOnly: true,
       maxAge: 3600000 * 5,
       secure: true,
-      sameSite: "none",
+      sameSite: 'none',
     })
-    .json({ msg: "Welcome admin" });
+    .json({ msg: 'Welcome admin' });
 });
 
 const loadAdmin = asyncHandler(async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select([
-      "-password",
-      "-verified",
-      "-googleLogin",
+      '-password',
+      '-verified',
+      '-googleLogin',
     ]);
     res.json(user);
   } catch (error) {
@@ -46,16 +46,16 @@ const loadAdmin = asyncHandler(async (req, res) => {
 
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find({}).select([
-    "-password",
-    "-verified",
-    "-secretCode",
-    "-googleLogin",
+    '-password',
+    '-verified',
+    '-secretCode',
+    '-googleLogin',
   ]);
   res.json({ users });
 });
 
 const getAllCheckOuts = asyncHandler(async (req, res) => {
-  const checkOuts = await CheckOut.find({}).sort("-createdAt");
+  const checkOuts = await CheckOut.find({}).sort('-createdAt');
   res.json(checkOuts);
 });
 
@@ -79,8 +79,8 @@ const getDetails = asyncHandler(async (req, res) => {
 
 const getRecentCheckout = asyncHandler(async (req, res) => {
   const checkOut = await CheckOut.find({})
-    .select("lineItems")
-    .sort("-createdAt")
+    .select('lineItems')
+    .sort('-createdAt')
     .limit(3);
   let lineItem = [];
   checkOut.map((c) => lineItem.push(c.lineItems));
@@ -93,28 +93,28 @@ const giveAdminPosition = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
   if (!user) {
-    throw new Error("No user found for this ID");
+    throw new Error('No user found for this ID');
   }
 
-  user.role = "admin";
+  user.role = 'admin';
   await user.save();
-  res.json({ msg: "Role Changed successfully" });
+  res.json({ msg: 'Role Changed successfully' });
 });
 
 const approveCheckout = asyncHandler(async (req, res) => {
   const checkOut = await CheckOut.findById(req.params.id);
   if (!checkOut) {
-    throw new Error("No checkout found for this ID");
+    throw new Error('No checkout found for this ID');
   }
   checkOut.isApproved = true;
   await checkOut.save();
-  res.json({ msg: "Approved succesfully" });
+  res.json({ msg: 'Approved succesfully' });
 });
 
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findByIdAndDelete(req.params.id);
   await product.save();
-  res.json({ msg: "Deleted Successfully" });
+  res.json({ msg: 'Deleted Successfully' });
 });
 
 const createProduct = asyncHandler(async (req, res) => {
@@ -148,7 +148,7 @@ const createProduct = asyncHandler(async (req, res) => {
     description,
   });
   await product.save();
-  res.json({ msg: "Created successfully" });
+  res.json({ msg: ' Product Created successfully' });
 });
 
 module.exports = {
