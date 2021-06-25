75144cb90e21cec7312e8c7325ecdb68d68181a5
Saral33 <princesaral2013@gmail.com>
2021-06-25 16:53:34 +0545

    Rating stars added

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,56 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import { Row,Col,Card, Button } from 'react-bootstrap'
-import {Link} from 'react-router-dom'
-import Loader from './Loader'
-import AlertMessage from './AlertMessage'
-import {useDispatch,useSelector} from 'react-redux'
-import { getProducts } from '../actions/productActions'
+import React, { useEffect, useState } from 'react';
+import { Row, Col, Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Loader from './Loader';
+import AlertMessage from './AlertMessage';
+import { useDispatch, useSelector } from 'react-redux';
+import Rating from './Ratings';
+import { getProducts } from '../actions/productActions';
 
 const Products = () => {
-    const [page,setPage] = useState(1)
-    const dispatch = useDispatch()
+  const [page, setPage] = useState(1);
+  const dispatch = useDispatch();
 
-    const {loading, remaining,products,error} = useSelector(state=> state.productsReducer)
+  const { loading, remaining, products, error } = useSelector(
+    (state) => state.productsReducer
+  );
 
-    useEffect(()=>{
-        dispatch(getProducts(page))
-    },[dispatch,page])
+  useEffect(() => {
+    dispatch(getProducts(page));
+  }, [dispatch, page]);
 
-    
-    
-    return (
-        <>
-         <h2 className='text-center'>Recommendations</h2>   
-         
-             {loading ? <Loader/> : error? <AlertMessage>{error}</AlertMessage> :
-             <Row>
-            { products.map(product=> (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3} className="d-flex align-items-stretch">
-                    <Card className='my-3 p-3'>
-                    <Link to={`/product/${product._id}`}>
-                    <Card.Img src={product.mainImage} />
-                     </Link>
-                   <Card.Body>
-                    <Card.Title as='h5' className='text-center'>
-                       <Link className='link' to={`/product/${product._id}`}>{product.name}</Link>
-                       </Card.Title>
-                    <Card.Text as='h5' className='text-center'>
-                       ${product.version[0]?.price}
-                       </Card.Text>
-                      </Card.Body>
-                   
-                    </Card>
-                </Col>
-            ))}
-            <Col className='d-flex justify-content-center my-4'>
-            { remaining > 0  &&  <Button onClick={()=> setPage(page + 1)}  variant='primary'>Load More</Button>}
-           </Col> 
+  return (
+    <>
+      <h2 className="text-center">Recommendations</h2>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <AlertMessage>{error}</AlertMessage>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col
+              key={product._id}
+              sm={12}
+              md={6}
+              lg={4}
+              xl={3}
+              className="d-flex align-items-stretch"
+            >
+              <Card className="my-3 p-3">
+                <Link to={`/product/${product._id}`}>
+                  <Card.Img src={product.mainImage} />
+                </Link>
+                <Card.Body>
+                  <Card.Title as="h5" className="text-center">
+                    <Link className="link" to={`/product/${product._id}`}>
+                      {product.name}
+                    </Link>
+                  </Card.Title>
+                  <Card.Text as="h5" className="text-center">
+                    ${product.version[0]?.price}
+                  </Card.Text>
+                  <Card.Text as="h4" className="text-center">
+                    <Rating value={product.ratings} />
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+          <Col className="d-flex justify-content-center my-4">
+            {remaining > 0 && (
+              <Button onClick={() => setPage(page + 1)} variant="primary">
+                Load More
+              </Button>
+            )}
+          </Col>
         </Row>
-             }
-            
-         
-        </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default Products
+export default Products;
