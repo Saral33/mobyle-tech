0df683ec2f03a9ce13eda3f3513aa620b1c1b540
Saral33 <princesaral2013@gmail.com>
2021-06-25 07:00:03 +0545

    small bug fix

--- Backend/Models/checkoutModel.js
+++ Backend/Models/checkoutModel.js
@@ -1,53 +1,60 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-
-const checkOutSchema = new mongoose.Schema({
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'user',
-        required:true
+const checkOutSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
     },
-    lineItems:[{
-        _id:false,
-        price_data:{
-            currency: {
-                type:String,
-                default: 'usd'
+    username: {
+      type: String,
+      required: true,
+    },
+    lineItems: [
+      {
+        _id: false,
+        price_data: {
+          currency: {
+            type: String,
+            default: 'usd',
+          },
+          product_data: {
+            name: {
+              type: String,
+              required: true,
             },
-            product_data:{
-                name:{
-                    type:String,
-                    required:true
-                },
-                images:[String]
-            },
-            unit_amount_decimal:{
-                type:Number,
-                required:true
-            },
-           
+            images: [String],
+          },
+          unit_amount_decimal: {
+            type: Number,
+            required: true,
+          },
         },
-        quantity:{
-            type:Number,
-            required:true
-        }
-    }],
-    isPaid:{
-        type:Boolean,
-        default:false
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    isPaid: {
+      type: Boolean,
+      default: false,
     },
-    isApproved:{
-        type:Boolean,
-        default:false
+    isApproved: {
+      type: Boolean,
+      default: false,
     },
-    address:{
-        type:String
+    address: {
+      type: String,
     },
-    totalamount:{
-        type: Number,
-        default: 0
-    }
-},{timestamps:true})
+    totalamount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-const CheckOut = mongoose.model('checkout',checkOutSchema)
-module.exports = CheckOut
+const CheckOut = mongoose.model('checkout', checkOutSchema);
+module.exports = CheckOut;
